1fb6360183614a89fe641a7e96b07d0cdeaba137
Tim BERNIC <nicolet.t@hotmail.com>
2025-06-03 08:35:22 +0200

    push

--- src/assets/Main/SectionBottom.jsx
+++ src/assets/Main/SectionBottom.jsx
@@ -8,7 +8,7 @@ const SectionBottom = ({ data }) => {
         {data.categories.map((element, index) => {
           if (element.meals.length !== 0) {
             return (
-              <div key={element + index} className="type-of-product-box">
+              <div key={element.name + index} className="type-of-product-box">
                 <h2>{element.name}</h2>
                 <ProductElementList element={element} />
               </div>
